15f93a5bf5c34ba1fc0ca500f71edba6cc39eb61
kory-io <kory@kehl.io>
2026-06-02 10:18:23 -0600

    feat(parser): expand type queries for TS, Python, Go, Java

--- queries/typescript_types.scm
+++ queries/typescript_types.scm
@@ -47,3 +47,21 @@
 (public_field_definition
   name: (property_identifier) @var.name
   type: (type_annotation (_) @var.type))
+
+; Object destructuring with type: const { foo }: SomeType = ...
+(variable_declarator
+  name: (object_pattern)
+  type: (type_annotation
+    (type_identifier) @var.name @var.type))
+
+; Array destructuring with type: const [a, b]: SomeType = ...
+(variable_declarator
+  name: (array_pattern)
+  type: (type_annotation
+    (type_identifier) @var.name @var.type))
+
+; Class property with new: class Foo { prop = new Bar() }
+(public_field_definition
+  name: (property_identifier) @ctor.name
+  value: (new_expression
+    constructor: (identifier) @ctor.type))
