e29b9163e8c8c01909a97a79eea91d15e8e06cc7
kory-io <kory@kehl.io>
2026-06-02 10:18:23 -0600

    feat(parser): expand type queries for TS, Python, Go, Java

--- queries/go_types.scm
+++ queries/go_types.scm
@@ -31,3 +31,19 @@
 (const_spec
   name: (identifier) @var.name
   type: (_) @var.type)
+
+; Short variable declaration with composite literal: x := Config{...}
+(short_var_declaration
+  left: (expression_list
+    (identifier) @ctor.name)
+  right: (expression_list
+    (composite_literal
+      type: (type_identifier) @ctor.type)))
+
+; Short variable declaration with qualified composite literal: x := pkg.Config{...}
+(short_var_declaration
+  left: (expression_list
+    (identifier) @ctor.name)
+  right: (expression_list
+    (composite_literal
+      type: (qualified_type) @ctor.type)))
